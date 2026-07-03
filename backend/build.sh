437ccd2515bab1661a36f4b1973ec981f2b5c785
Asquare Technologies <asquaremails@gmails.com>
2026-07-03 09:34:38 +0530

    Add Superuser

--- backend/build.sh
+++ backend/build.sh
@@ -2,5 +2,16 @@
 set -o errexit
 
 pip install -r requirements.txt
+
+python manage.py shell -c "
+from accounts.models import User;
+User.objects.filter(email='admin@asquaretechnologiesweb.com').exists() or User.objects.create_superuser(
+email='admin@asquaretechnologiesweb.com',
+password='Admin@asquare123',
+first_name='Admin',
+last_name='Asquare',
+role=User.Role.ADMIN
+)
+
 python manage.py collectstatic --no-input
 python manage.py migrate
